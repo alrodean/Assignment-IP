1f9835f862028ad67b8d7d5b5457ef37ec42aa16
Deen <deenfransman@gmail.com>
2026-04-19 15:14:12 +0200

    ...

--- library.php
+++ library.php
@@ -112,6 +112,7 @@ function Library(){
                         $duration = (int)$duration;
 
                         $book = [
+                            'Borrow ID' => 1,
                             'Book Title' => $bookName,
                             'Category' => $catType,
                             "Allowed Days" => $duration,
